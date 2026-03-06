d8b4c976df2ee34f83a47143d01c1d667e6ef740
Achsan <achsanh@gmail.com>
2026-03-06 23:42:01 +0000

    🎉 feat: initialize Nuxt project

---
+++ apps/noties-app/app/app.config.ts
@@ -0,0 +1,12 @@
+export default defineAppConfig({
+  ui: {
+    // https://ui.nuxt.com/docs/getting-started/theme/design-system#runtime-configuration
+    colors: {
+      primary: "blue",
+      secondary: "sky",
+      tertiary: "slate",
+      info: "cyan",
+      neutral: "gray",
+    },
+  },
+});
---
+++ apps/noties-app/nuxt.config.ts
@@ -0,0 +1,33 @@
+import { isDevelopment } from "std-env";
+
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: "2025-07-15",
+  devtools: { enabled: isDevelopment },
+  sourcemap: isDevelopment,
+  future: { compatibilityVersion: 5 },
+  experimental: { nitroAutoImports: true },
+  modules: ["@nuxt/ui"],
+  ui: {
+    theme: {
+      // https://ui.nuxt.com/docs/getting-started/theme/design-system#extend-colors
+      colors: [
+        "primary",
+        "secondary",
+        "tertiary",
+        "info",
+        "success",
+        "warning",
+        "error",
+      ],
+      defaultVariants: { size: "sm" },
+      prefix: "tw",
+    },
+    content: true,
+    experimental: { componentDetection: true },
+  },
+  fonts: {
+    processCSSVariables: true,
+  },
+  css: ["~/assets/css/main.css"],
+});
