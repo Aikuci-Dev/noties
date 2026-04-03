5951da85513c4db9031614d13f381e4f5a29f105
Achsan <achsanh@gmail.com>
2026-04-03 11:35:53 +0100

    🏷️ refactor: enhance x6 person data structures

--- apps/noties-app/app/utils/types/person.ts
+++ apps/noties-app/app/utils/types/person.ts
@@ -16,37 +16,61 @@ export type PersonMeta = {
   generationOrder?: number;
 };
 export type PersonWithMeta = Person & { meta: PersonMeta };
-export type PersonMap<T extends Person | NodePerson = Person> = Map<T["id"], T>;
-export type People<T extends Person & PersonMeta | NodePerson & NodePersonMeta = Person> = T[];
-export type PeopleWithMeta<T extends PersonWithMeta | NodePersonWithMeta = PersonWithMeta> = T[];
-export type PeopleByRank<T extends Person | NodePerson = Person> = { [rank: number]: People<T> };
 
-export type PersonRelationship<T extends Person | NodePerson = Person> = {
+export type PersonMap<T extends Person | PersonNode = Person> = Map<T["id"], T>;
+export type PersonRelationship<T extends Person | PersonNode = Person> = {
   id: T["id"];
   partnerId: T["id"];
   partnerIds: T["id"][];
   childrenIds: T["id"][];
 };
-export type PersonRelationshipMap<T extends Person | NodePerson = Person> = Map<
+export type PersonRelationshipMap<T extends Person | PersonNode = Person> = Map<
   PersonRelationship<T>["id"],
   PersonRelationship<T>
 >;
-export type PersonRelationshipPartner<T extends Person | NodePerson = Person> = {
+export type PersonRelationshipPartner<T extends Person | PersonNode = Person> = {
   id: EntityPairKey<T>;
   personId: T["id"];
   partnerId: T["id"];
   childrenIds: T["id"][];
 };
-export type PersonRelationshipPartnerMap<T extends Person | NodePerson = Person> = Map<
+export type PersonRelationshipPartnerMap<T extends Person | PersonNode = Person> = Map<
   PersonRelationshipPartner<T>["id"],
   PersonRelationshipPartner<T>
 >;
 
+export type People<T extends Person | PersonNode = Person> = T[];
+export type PeopleWithMeta<T extends PersonWithMeta | PersonNodeWithMeta = PersonWithMeta> = T[];
+export type PeopleByRank<T extends Person | PersonNode = Person> = { [rank: number]: People<T> };
+
 /** ==== X6 ==== */
+// X6 Cell
+export type PersonCellData = PersonNodeData | PersonEdgeData;
+export type PersonNodeData =
+  & { personType: AllowedPersonNodeType }
+  & (
+    | { type: "NODE_PERSON" } & NodePersonData
+    | { type: "NODE_PERSON_RELATIONSHIP" } & NodePersonRelationshipData
+    | { type: "NODE_PERSON_PLACEHOLDER" } & NodePersonPlaceholderData
+  );
+export type PersonEdgeData =
+  & { personType: AllowedPersonEdgeType }
+  & { type: "EDGE_LINE" }
+  & EdgePersonData;
+
 // X6 Node
-const NODE_TYPE = ["PERSON", "PERSON_RELATIONSHIP"] as const;
-export type NodeType = typeof NODE_TYPE[number];
-export type NodePerson = Omit<Person, "name" | "dateOfBirth" | "dateOfDeath"> & {
+export const NODE_PERSON = "node-person";
+export const NODE_PERSON_STACK = "node-person-stack";
+export const NODE_PERSON_PLACEHOLDER = "node-person-placeholder";
+export const NODE_PERSON_INTERMEDIARY = "node-person-intermediary";
+
+export const BUTTON_NODE_EDIT_PERSON = "button-edit-person";
+
+const PERSON_NODE_TYPE = ["PERSON", "PERSON_RELATIONSHIP"] as const;
+export type PersonNodeType = typeof PERSON_NODE_TYPE[number];
+export type AllowedPersonNodeType = PersonNodeType;
+
+export type PersonNode = Omit<Person, "name" | "dateOfBirth" | "dateOfDeath"> & {
   title: string;
   subtitle?: string;
   generationOrder?: number;
@@ -55,33 +79,18 @@ export type NodePerson = Omit<Person, "name" | "dateOfBirth" | "dateOfDeath"> & 
     original?: Person;
   };
 };
-export type NodePersonMeta = {
+export type PersonNodeMeta = {
   isStack?: boolean;
 };
-export type NodePersonWithMeta = NodePerson & { meta: NodePersonMeta };
-export type NodePersonData = { value: NodePerson; original: Person | PersonWithMeta; meta?: NodePersonMeta };
-
-export const NODE_PLACEHOLDER = "node-placeholder";
-export const NODE_INTERMEDIARY = "node-intermediary";
-export const NODE_PERSON = "node-person";
-export const NODE_PERSON_STACK = "node-person-stack";
-export const NODE_PERSON_PLACEHOLDER = "node-person-placeholder";
-export const NODE_PERSON_INTERMEDIARY = "node-person-intermediary";
-
-export const BUTTON_NODE_EDIT_PERSON = "button-edit-person";
+export type PersonNodeWithMeta = PersonNode & { meta: PersonNodeMeta };
 
 // X6 Edge
-const EDGE_TYPE = ["LINE", "PARTNER"] as const;
-export type EdgeType = typeof EDGE_TYPE[number];
-export type EdgeLineMeta = {
-  isDash?: boolean;
+const PERSON_EDGE_TYPE = ["PERSON", "PARTNER"] as const;
+export type PersonEdgeType = typeof PERSON_EDGE_TYPE[number];
+export type AllowedPersonEdgeType = PersonEdgeType;
+
+export type PersonEdgeMeta = BaseEdgeMeta & {
   isPlaceholder?: boolean;
   hasPartner?: boolean;
   isChildOfCurrentMarriage?: boolean;
 };
-
-export const EDGE_LINE = "edge-line";
-export const EDGE_LINE_DASH = "edge-line-dash";
-
-// X6 Cell
-export type PersonCellData<TMeta = unknown> = CellData<NodeType, EdgeType, TMeta>;
--- apps/noties-app/app/utils/types/x6.ts
+++ apps/noties-app/app/utils/types/x6.ts
@@ -1,9 +1,17 @@
-import type { Node as X6Node } from "@antv/x6";
+import type { Cell, Node as X6Node } from "@antv/x6";
 
-export type CellData<TNodeType, TEdgeType, TMeta = unknown> =
-  | { cellType: "NODE"; type: TNodeType; value: unknown; meta?: TMeta }
-  | { cellType: "EDGE"; type: TEdgeType; value: unknown; meta?: TMeta };
+// Cell
+export type AllowedCellData = PersonCellData;
 
-export type AllowedCellData<TMeta = unknown> = PersonCellData<TMeta>;
+// Node
+export const NODE_PLACEHOLDER = "node-placeholder";
+export const NODE_INTERMEDIARY = "node-intermediary";
 
 export type NodeWithChildrenNodeMap<T extends Id> = Map<T, { node: X6Node; nodeChildren: X6Node[] }>;
+
+// Edge
+export const EDGE_LINE = "edge-line";
+export const EDGE_LINE_DASH = "edge-line-dash";
+
+export type BaseEdgeValue = { source: Cell; target: Cell };
+export type BaseEdgeMeta = { type: "default" | "dash" };
