165a6e3030d54e9a205b45182b22ea13cbb453b9
Achsan <achsanh@gmail.com>
2026-05-05 01:58:33 +0100

    ✨ feat: integrate VueUse focus trap

--- apps/nuxt/app/components/ui/form/Wrapper.vue
+++ apps/nuxt/app/components/ui/form/Wrapper.vue
@@ -1,6 +1,8 @@
 <template>
   <Form :of @submit="(values) => $emit('submit', values)">
-    <slot v-bind="{ of }" />
+    <div ref="formFieldsEl">
+      <slot v-bind="{ of }" />
+    </div>
     <slot name="action-buttons">
       <div class="tw:flex tw:justify-end">
         <button class="tw:d-btn tw:d-btn-success">Submit</button>
@@ -13,10 +15,17 @@
 import type { FormStore, Schema } from "@formisch/vue";
 
 import { Form } from "@formisch/vue";
+import { useFocusTrap } from "@vueuse/integrations/useFocusTrap";
 
 interface FormProps<TSchema extends Schema = Schema> {
   of: FormStore<TSchema>;
 }
 const { of } = defineProps<FormProps<TSchema>>();
 defineEmits(["submit"]);
+
+const formFieldsEl = useTemplateRef("formFieldsEl");
+
+const { activate, deactivate } = useFocusTrap(formFieldsEl, { allowOutsideClick: true });
+
+defineExpose({ focusTrap: { activate, deactivate } });
 </script>
